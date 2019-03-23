796d4ee1326495fc00b3552d8e90fcd0fbcbc849
wangchangjing <1071499071@qq.com>
2019-03-23 10:59:36 +0800

    2019年3月23日10:59:33

--- src/main/java/com/demo/io/IOServer.java
+++ src/main/java/com/demo/io/IOServer.java
@@ -13,14 +13,13 @@ import java.net.Socket;
  */
 public class IOServer {
     public static void main(String[] args) throws Exception {
-
+        // 创建一个ServerSocket来监听 8000 端口
         ServerSocket serverSocket = new ServerSocket(8000);
 
-        // (1) 接收新连接线程
         new Thread(() -> {
             while (true) {
                 try {
-                    // (1) 阻塞方法获取新的连接
+                    // (1) 阻塞方法获取新的连接，如果没有新连接，就一直在等待
                     Socket socket = serverSocket.accept();
 
                     // (2) 每一个新的连接都创建一个线程，负责读取数据
--- src/main/java/com/demo/netty/simple/NettyServer.java
+++ src/main/java/com/demo/netty/simple/NettyServer.java
@@ -19,8 +19,8 @@ public class NettyServer {
     public static void main(String[] args) {
         ServerBootstrap serverBootstrap = new ServerBootstrap();
 
-        NioEventLoopGroup boss = new NioEventLoopGroup();
-        NioEventLoopGroup worker = new NioEventLoopGroup();
+        NioEventLoopGroup boss = new NioEventLoopGroup(); // 对应 IOServer.java 中的接受新连接线程，主要负责创建新连接
+        NioEventLoopGroup worker = new NioEventLoopGroup(); // 对应 IOServer.java 中的负责读取数据的线程，主要用于读取数据以及业务逻辑处理
         serverBootstrap
                 .group(boss, worker)
                 .channel(NioServerSocketChannel.class)
