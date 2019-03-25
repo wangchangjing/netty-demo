35a5a1d632576941aebc4253486d328fa58cea14
wangchangjing <1071499071@qq.com>
2019-03-25 21:56:54 +0800

    添加 客户端与服务端双向通信

--- src/main/java/com/demo/netty/client/NettyClient.java
+++ src/main/java/com/demo/netty/client/NettyClient.java
@@ -1,4 +1,4 @@
-package com.demo.netty;
+package com.demo.netty.client;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelInitializer;
@@ -6,19 +6,19 @@ import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
-import io.netty.util.AttributeKey;
 
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 /**
- * @Description:
- * @ClassName: NettyClient
- * @Author WangChangJing
- * @Date 2019/3/1
+ * @author WangChangJing
+ * @description:
+ * @date 2019-03-25
  */
 public class NettyClient {
     private static final int MAX_RETRY = 5;
+    private static final String HOST = "127.0.0.1";
+    private static final int PORT = 8000;
 
 
     public static void main(String[] args) {
@@ -26,25 +26,19 @@ public class NettyClient {
 
         Bootstrap bootstrap = new Bootstrap();
         bootstrap
-                // 1.指定线程模型
                 .group(workerGroup)
-                // 2.指定 IO 类型为 NIO
                 .channel(NioSocketChannel.class)
-                // 绑定自定义属性到 channel
-                .attr(AttributeKey.newInstance("clientName"), "nettyClient")
-                // 设置TCP底层属性
                 .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
                 .option(ChannelOption.SO_KEEPALIVE, true)
                 .option(ChannelOption.TCP_NODELAY, true)
-                // 3.IO 处理逻辑
                 .handler(new ChannelInitializer<SocketChannel>() {
                     @Override
                     public void initChannel(SocketChannel ch) {
+                        ch.pipeline().addLast(new FirstClientHandler());
                     }
                 });
 
-        // 4.建立连接
-        connect(bootstrap, "127.0.0.1", 8000, MAX_RETRY);
+        connect(bootstrap, HOST, PORT, MAX_RETRY);
     }
 
     private static void connect(Bootstrap bootstrap, String host, int port, int retry) {
---
+++ src/main/java/com/demo/netty/server/FirstServerHandler.java
@@ -0,0 +1,38 @@
+package com.demo.netty.server;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.nio.charset.Charset;
+import java.util.Date;
+
+/**
+ * @author WangChangJing
+ * @description:
+ * @date 2019-03-25
+ */
+public class FirstServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf byteBuf = (ByteBuf) msg;
+
+        System.out.println(new Date() + ": 服务端读到数据 -> " + byteBuf.toString(Charset.forName("utf-8")));
+
+        // 回复数据到客户端
+        System.out.println(new Date() + ": 服务端写出数据");
+        ByteBuf out = getByteBuf(ctx);
+        ctx.channel().writeAndFlush(out);
+    }
+
+    private ByteBuf getByteBuf(ChannelHandlerContext ctx) {
+        byte[] bytes = "你好，这里是服务端！".getBytes(Charset.forName("utf-8"));
+
+        ByteBuf buffer = ctx.alloc().buffer();
+
+        buffer.writeBytes(bytes);
+
+        return buffer;
+    }
+}
